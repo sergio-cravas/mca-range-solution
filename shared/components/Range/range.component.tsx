ba98dbffeb585133740fc49a59ea3428ddb1b829
Sergio Cerdá Hervás <sergio.cravas@gmail.com>
2024-05-08 21:38:37 +0200

    feat: WIP on slider functionallity and improved styles

--- shared/components/Range/range.component.tsx
+++ shared/components/Range/range.component.tsx
@@ -1,3 +1,4 @@
+import { useRef, useState } from 'react';
 import styles from './range.module.scss';
 
 type Props = {
@@ -8,14 +9,58 @@ type Props = {
 };
 
 const Range = ({ label, rangeValues, onChangeRangeMin, onChangeRangeMax }: Props) => {
+  const inputRef = useRef<HTMLDivElement>(null);
+
+  const [minValue, setMinValue] = useState<number>(rangeValues[0].value || 0);
+  const [maxValue, setMaxValue] = useState<number>(rangeValues[rangeValues.length - 1].value || 100);
+  const [isDragging, setIsDragging] = useState<boolean>(false);
+
+  const handleOnMoveMin = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (isDragging && inputRef.current) {
+      const rect = inputRef.current.getBoundingClientRect();
+      const x = event.clientX - rect.left;
+      const percent = (x / rect.width) * 100;
+
+      const newValue = Math.max(0, Math.min(maxValue, Math.round((percent / 100) * 100)));
+      setMinValue(newValue);
+    }
+  };
+
+  const handleOnMoveMax = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (isDragging && inputRef.current) {
+      const rect = inputRef.current.getBoundingClientRect();
+      const x = event.clientX - rect.left;
+      const percent = (x / rect.width) * 100;
+
+      const newValue = Math.max(minValue, Math.min(100, Math.round((percent / 100) * 100)));
+      setMaxValue(newValue);
+    }
+  };
+
   return (
     <div className={styles.range}>
       <label className={styles.range__label}>{label}</label>
 
-      <div className={styles.range__input}>
-        <div className={styles['range__input__bullet']} style={{ left: '50%' }} />
+      <div
+        ref={inputRef}
+        className={styles.range__input}
+        onMouseUp={() => setIsDragging(false)}
+        onMouseDown={() => setIsDragging(true)}
+      >
+        <div
+          className={styles.range__input__selection}
+          style={{ left: `${minValue}%`, width: `calc(${maxValue}% - ${minValue}%)` }}
+        />
 
-        <div className={styles['range__input__bullet']} style={{ left: '0%' }} />
+        <div onMouseMove={handleOnMoveMin} className={styles.range__input__bullet} style={{ left: `${minValue}%` }} />
+
+        <div onMouseMove={handleOnMoveMax} className={styles.range__input__bullet} style={{ left: `${maxValue}%` }} />
+
+        <div className={styles['range__input__range-values']}>
+          {rangeValues.map((value, index) => (
+            <span key={`range_value-label_${index}`}>{value.label}</span>
+          ))}
+        </div>
       </div>
     </div>
   );
