b17c3d51d32b278404758eaa5cc07e131096900e
Sergio Cerdá Hervás <sergio.cravas@gmail.com>
2024-05-10 09:13:16 +0200

    feat: Updated styles of header links

--- shared/components/Header/header.component.tsx
+++ shared/components/Header/header.component.tsx
@@ -5,9 +5,17 @@ import styles from './header.module.scss';
 const Header = () => {
   return (
     <header className={styles.header}>
-      <Link href="/">Home</Link>
-      <Link href="/exercise1">Exercise 1</Link>
-      <Link href="/exercise2">Exercise 2</Link>
+      <Link className={styles.header__link} href="/">
+        Home
+      </Link>
+
+      <Link className={styles.header__link} href="/exercise1">
+        Exercise 1
+      </Link>
+
+      <Link className={styles.header__link} href="/exercise2">
+        Exercise 2
+      </Link>
     </header>
   );
 };
