63892e5004b3362bfec91cda8e35da8ed5c865b5
Sergio Cerdá Hervás <sergio.cravas@gmail.com>
2024-05-13 09:22:56 +0200

    hot-fix: Remove invalid async testing and added missing specific tests

--- cypress/e2e/exercise2.cy.ts
+++ cypress/e2e/exercise2.cy.ts
@@ -1,9 +1,9 @@
 describe('template spec', () => {
-  before(() => {
+  beforeEach(() => {
     cy.visit('/exercise2');
   });
 
-  it('Check if range input renders and recieves correct props', async () => {
+  it('Check if range input renders and recieves correct props', () => {
     const requerimentsProps = {
       min: 1.99,
       max: 70.99,
@@ -13,5 +13,24 @@ describe('template spec', () => {
       .should('have.attr', 'aria-valuemin', String(requerimentsProps.min))
       .should('have.attr', 'aria-valuemax', String(requerimentsProps.max))
       .should('have.attr', 'aria-valuetext', `Range between ${requerimentsProps.min} and ${requerimentsProps.max}`);
+  });
+
+  it('Move min thumn', () => {
+    const requerimentsProps = {
+      min: 1.99,
+      max: 70.99,
+    };
+
+    cy.get("[data-testid='minimum-thumb']")
+      .should('be.visible')
+      .trigger('mousedown', { which: 1 })
+      .trigger('mousemove', { clientX: 500 })
+      .trigger('mouseup', {});
+
+    cy.get('[role="slider"]').should(($slider) => {
+      const value = $slider.attr('aria-valuetext');
+
+      expect(value).not.to.eq(`Range between ${requerimentsProps.min} and ${requerimentsProps.max}`);
+    });
   });
 });
